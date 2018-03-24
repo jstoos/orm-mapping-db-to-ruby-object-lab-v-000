2250241fd997fc3a33f67e2c872b5f7b3bf2fc0a
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-24 23:47:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
 
   def self.find_by_name(name)
     # find the student in the database given a name
-    # return a new instance of the Student class
+    self.new_from_db(row)
   end
 
   def save
