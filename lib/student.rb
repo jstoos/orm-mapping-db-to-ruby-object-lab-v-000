16d34260bfd6598197ce7e6d6125253519eac71b
Jennifer Stoos <jstoos@bitlaw.com>
2018-03-25 00:18:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -114,8 +114,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10
-      LIMIT 1
+      WHERE id = 1
     SQL
 
     DB[:conn].execute(sql).map do |row|
